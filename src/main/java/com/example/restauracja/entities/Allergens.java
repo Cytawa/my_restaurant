0de4fb53dbf18c9f63b6793b7071cfd475258dc9
Cytawa <tomasz.poniatowski@cytawa.com>
2022-04-04 18:48:52 +0200

    1

---
+++ src/main/java/com/example/restauracja/entities/Allergens.java
@@ -0,0 +1,8 @@
+package com.example.restauracja.entities;
+
+public enum Allergens {
+    MILK,
+    GLUTEN,
+    SEAFOOD,
+    NUTS
+}
---
+++ src/main/java/com/example/restauracja/entities/DishType.java
@@ -0,0 +1,7 @@
+package com.example.restauracja.entities;
+
+public enum DishType {
+    VEGAN,
+    VEGETARIAN,
+    NON_VEGAN
+}
